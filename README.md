cab51cbe828954876661808af52ae7ac69b819b0
Parth Shah <parthvshah@gmail.com>
2019-06-19 12:52:32 +0530

    WIP par2.1.c

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ Serial and parallel implementation of Velocity Auto-Correlation Function
 
 ## Naming
 - ```par1.c``` - Correlation decomposition
-- ```par2.c``` - Correlation decomposition and particle decomposition (experimental)
 - ```par1.1.c``` - Correlation decomposition, load balancing
-- ```par1.3.c``` - Particle decomposition
+- ```par1.3.c``` - Particle decomposition, barrier implementation
 - ```par1.3.1.c``` - Particle decomposition, lesser communication
+- ```par2.c``` - Correlation decomposition and particle decomposition, sub-root processes
+- ```par2.1.c``` - Correlation decomposition and particle decomposition, batch processing
 - ```parOpenMP.c``` - OpenMP implementation, correlation decomposition
 - ```seq.c``` - Sequential implementation 
 
--- par2.1.c
+++ par2.1.c
@@ -1,5 +1,5 @@
 /*
-Calculate Velocity Auto-Correlation (Serial)
+Calculate Velocity Auto-Correlation (Parallel)
 This program calculates the VACF from a given HISTORY file. Scripts to clean the data,
 plot the graph and calculate the coefficient of diffusion are external and in python.
 
@@ -27,7 +27,7 @@ Algorithm:
             accumalate += particle
         print(dt, accumalate/((N-1)*count)
 
-Date: 6-6-2019
+Date: 10-6-2019
  */
 
 #include <stdio.h>
@@ -75,12 +75,21 @@ void readData(int start, int step, char *fileName)
 
 int main(int argc, char **argv)
 {
-    int rank, wSize, NStart, NEnd, chunk, remainder;
+    int wRank, wSize, lChunk, NChunk, lRemainder, NRemainder, NStart, NEnd, lStart, lEnd;
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &wRank);
     MPI_Comm_size(MPI_COMM_WORLD, &wSize);
+
+    int colour = wRank / 12;
+    MPI_Comm MPI_COMM_ROW;
+    MPI_Comm_split(MPI_COMM_WORLD, colour, wRank, &MPI_COMM_ROW);
+
+    int rRank, rSize;
+    MPI_Comm_rank(MPI_COMM_ROW, &rRank);
+    MPI_Comm_size(MPI_COMM_ROW, &rSize);
 
     int start;
     int stop;
@@ -91,7 +100,7 @@ int main(int argc, char **argv)
     double accumalate, particle;
     int count;
 
-    if (rank == 0)
+    if (wRank == 0)
     {
         char fileName[256];
         for (int c = 1; c < argc; c++)
@@ -120,11 +129,12 @@ int main(int argc, char **argv)
         readData(start, step, fileName);
 
         fprintf(stdout, "timestep, vacf\n");
+
     }
 
-    MPI_Bcast(&tmax, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&tmax, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&xData, ROW * COL, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&yData, ROW * COL, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&zData, ROW * COL, MPI_DOUBLE, 0, MPI_COMM_WORLD);
@@ -135,25 +145,35 @@ int main(int argc, char **argv)
     double *gCorr = (double *)malloc(sizeof(double) * (tmax + 1));
     gCorr[0] = 0.0;
 
-    // Atom calculation
-    chunk = (N - 1) / wSize;
-    remainder = (N - 1) - (chunk * wSize);
-    NStart = (rank * chunk) + 1;
-    NEnd = NStart + chunk - 1;
+    // Correlation calculation
+    lChunk = tmax / rSize;
+    lRemainder = tmax - (lChunk * rSize);
+    lStart = ((wRank - (wRank%12)) / 12) * lChunk + 1;
+    lEnd = lStart + lChunk - 1;
 
-    if((remainder != 0) && (rank == (wSize-1)))
+    if((lRemainder != 0) && (((wRank - (wRank%12)) / 12) == 84))
     {
-        NEnd += remainder;
+        lEnd += lRemainder;
     }
 
-    for (int dt = 1; dt <= tmax; dt++)
+    // Atom calculation
+    NChunk = (N - 1) / rSize;
+    NRemainder = (N - 1) - (NChunk * rSize);
+    NStart = (rRank * NChunk) + 1;
+    NEnd = NStart + NChunk - 1;
+
+    if((NRemainder != 0) && (rRank == (rSize-1)))
+    {
+        NEnd += NRemainder;
+    }
+
+    for (int dt = lStart; dt <= lEnd; dt++)
     {
         count = 0;
         accumalate = 0.0;
         for (int t = 0; t < (M - dt); t++)
         {
             particle = 0.0;
-            // for (int i = 1; i < N; i++)
             for (int i = NStart; i <= NEnd; i++)
             {
                 particle += xData[i][t] * xData[i][t + dt] +
@@ -168,16 +188,17 @@ int main(int argc, char **argv)
         // fprintf(stdout, "%d, %e\n", dt, accumalate);
     }
 
-    MPI_Reduce(lCorr, gCorr, tmax + 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    if (rank == 0)
+    MPI_Reduce(lCorr, gCorr, tmax + 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_ROW);
+    
+    if (rRank == 0)
     {
         for (int k = 1; k <= tmax; k++)
         {
-            fprintf(stdout, "%d, %e\n", k, gCorr[k]);
+            fprintf(stdout, "%d, %e\n", ((wRank/12)*lChunk) + k, gCorr[k]);
         }
     }
 
     MPI_Finalize();
+    
     return 0;
 }
